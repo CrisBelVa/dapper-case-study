e33a86b35b939eeb0ad263d0da9dbf52a1f1cabf
CrisBelVa <cristhian.beltran92@gmail.com>
2025-10-08 15:02:22 +0100

    Update 3_Prevention_&_Execution.py

--- pages/3_Prevention_&_Execution.py
+++ pages/3_Prevention_&_Execution.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-from app import card_start, card_end, kpi_chip, inject_google_css
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
+from ui import card_start, card_end, kpi_chip, inject_google_css
 
-
+# DO NOT call st.set_page_config() in pages
+import streamlit as st
+inject_google_css()
 
 st.title("Part 3 – Prevention & Execution")
 
